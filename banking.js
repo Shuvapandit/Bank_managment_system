498c57c5a5ee3b7b6827242758293c2285d9db36
shuva <chakrabortyshuva216@gmail.com>
2022-02-11 21:47:35 +0600

    commit deposit widthdraw functionality updated

--- banking.js
+++ banking.js
@@ -2,12 +2,24 @@
   document.getElementById('deposit-btn').addEventListener('click',function(){
       
       const depoinput=document.getElementById('depo_amount');
-      const depoamount=depoinput.value;
-      console.log(depoamount)
+      const newdepoamountText=depoinput.value;
+      const newdepoAmount=parseFloat(newdepoamountText);
+     
       const depototal=document.getElementById('dpottl');
-    depototal.innerText= depoamount;
+      const predepoamountText=depototal.innerText;
+      const predepoamountAmount=parseFloat(predepoamountText);
+    const newdepositTotal= predepoamountAmount+ newdepoAmount;
+    depototal.innerText=newdepositTotal;
+     //update account balance  total
+     const balancetotal=document.getElementById('Balance-total');
+     const balanceTotalText=balancetotal.innerText;
+     const prebalancetotal=parseFloat(balanceTotalText);
+     const newBalanceTotal=prebalancetotal+newdepoAmount;
+     balancetotal.innerText=newBalanceTotal;
     //clear  depo input field
     depoinput.value='';
+
+   
     
   
    
@@ -17,10 +29,22 @@
     //widthdraw
     document.getElementById('widthdraw-btn').addEventListener('click',function(){
     const widthdrawInput=document.getElementById('widthdraw_amount');
-    const widthdrawAmount=widthdrawInput.value;
-    console.log(widthdrawAmount)
+    const widthdrawAmountText=widthdrawInput.value;
+    const newwidthdrawAmount=parseFloat( widthdrawAmountText);
+
     const widthdrawtotal=document.getElementById('widthdrawtotal');
-    widthdrawtotal.innerText= widthdrawAmount;
+    const prewidthdrawText=widthdrawtotal.innerText;
+    const newPrewidthdraw=parseFloat(prewidthdrawText);
+    const newWidthdrawTotal=newPrewidthdraw+ newwidthdrawAmount;
+    widthdrawtotal.innerText=newWidthdrawTotal;
+  //update widthdraw account balance  total
+  const balancetotalwidthdraw=document.getElementById('Balance-total');
+  const balancetotalwidthdrawText=balancetotalwidthdraw.innerText;
+  const prewidthdrawbalancetotal=parseFloat(balancetotalwidthdrawText);
+  const newwidthdrawBalanceTotal=prewidthdrawbalancetotal-newwidthdrawAmount;
+  balancetotalwidthdraw.innerText=newwidthdrawBalanceTotal;
+  
+
     //clear widthdraw input field
     widthdrawInput.value='';
 
